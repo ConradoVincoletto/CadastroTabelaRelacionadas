cd931cded6945c6390b86041592f031c2f13e689
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-03-16 19:09:43 -0300

    feat: IAaction Create, e a view, e o IAaction Create para retornar a lista de produtos

--- CadastroTabelasRelacionadas/Controllers/ProdutosController.cs
+++ CadastroTabelasRelacionadas/Controllers/ProdutosController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CadastroTabelasRelacionadas.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CadastroTabelasRelacionadas.Controllers
 {
     public class ProdutosController : Controller
     {
+        public static List<Produto> listaProdutos = new List<Produto>();
         public IActionResult Index()
         {
+            return View(listaProdutos);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Produto objeto)
+        {
+            listaProdutos.Add(objeto);
+            return RedirectToAction("Index"); 
         }
     }
 }
