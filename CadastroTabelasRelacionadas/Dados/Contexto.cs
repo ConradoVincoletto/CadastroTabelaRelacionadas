243a3ef43764add99138442889608c2592ab89c0
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-03-17 18:41:15 -0300

    feat: Classe contexto

---
+++ CadastroTabelasRelacionadas/Dados/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using CadastroTabelasRelacionadas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroTabelasRelacionadas.Contexto
+{
+    public class Contexto : DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+        public DbSet<Produto> produtos { get; set; }
+
+    }
+}
