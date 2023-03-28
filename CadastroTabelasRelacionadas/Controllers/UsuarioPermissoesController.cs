ba37dde5258f16fdb74f6be4a2e39c9e92d9a125
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-03-28 17:24:41 -0300

    feat: controller Usuários_Permissão

---
+++ CadastroTabelasRelacionadas/Controllers/UsuarioPermissoesController.cs
@@ -0,0 +1,26 @@
+﻿using CadastroTabelasRelacionadas.Dados;
+using CadastroTabelasRelacionadas.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroTabelasRelacionadas.Controllers
+{
+    public class UsuarioPermissoesController : Controller
+    {
+        private readonly Contexto db;
+
+        public UsuarioPermissoesController(Contexto contexto)
+        {
+            db = contexto;
+        }
+        [HttpGet("[controller]/[action]/{UsuarioId}/{PermissaoId}")]
+        public IActionResult AdicionarPermissao(int UsuarioId, int PermissaoId)
+        {
+            Usuarios_Permissoes novo = new Usuarios_Permissoes();
+            novo.UsuarioId = UsuarioId;
+            novo.PermissaoId = PermissaoId;
+            db.usuario_permissao.Add(novo);
+            db.SaveChanges();
+            return Redirect("/Usuarios");
+        }
+    }
+}
