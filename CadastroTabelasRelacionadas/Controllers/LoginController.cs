e604ebd3c09b81c9a79d3f5319a707f4ac5552e5
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-03-31 17:37:29 -0300

    feat: controller Login

---
+++ CadastroTabelasRelacionadas/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroTabelasRelacionadas.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Entrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Entrar(string login, string senha)
+        {
+            return Redirect("/");
+        }
+
+        public async Task<IActionResult> Logoff()
+        {
+            await HttpContext.SignOutAsync("CookieAuthentication");
+            return Redirect("/Login/Entrar");
+        }
+    }
+}
